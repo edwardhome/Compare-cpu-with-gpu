9961ee0b2fbd0d9d64f31153d663d860d30fb182
Defu <edward61221@gmail.com>
2020-07-22 13:18:02 +0800

    修改
    修改TF視窗開啟大小

---
+++ main.py
@@ -0,0 +1,39 @@
+import time
+from matplotlib import pyplot as plt
+import tensorflow as tf
+
+def DeviceTest(device_name,size,sess):
+    with tf.device(device_name):       
+        M1=tf.random_normal([size,size],name='M1')
+        M2=tf.random_normal([size,size],name='M2')
+        mul=tf.matmul(M1,M2,name='mul')
+        sum_result=tf.reduce_sum(mul,name='sum_result')
+        start_time=time.time()
+        result=sess.run(sum_result)
+        take_time=time.time()-start_time       
+        #print(device_name,"  size=",size, " Time:",take_time)
+        return take_time  #這樣append到SET才有東西
+
+computation = input('請輸入最大值＝ ')
+computation = int(computation)+1
+step = int((computation-1)/10)
+
+with tf.Session() as sess:
+    Gpu_Set=[];  Cpu_set=[];  Size_set=[]
+    for i in range(0,computation,step):
+        TestGpu=DeviceTest("/gpu:0",i,sess)
+        TestCpu=DeviceTest("/cpu:0",i,sess)
+        Gpu_Set.append(TestGpu)
+        Cpu_set.append(TestCpu)
+        Size_set.append(i)
+
+fig=plt.gcf()
+fig.set_size_inches(10,5)
+plt.plot(Size_set, Gpu_Set ,label='GPU')
+plt.plot(Size_set, Cpu_set ,label='CPU')
+plt.legend()
+plt.title('Compare CPU with GPU')
+plt.xlabel('Computation DATA')
+plt.ylabel('Computation Time (sec)')
+plt.savefig('Testing.png')
+plt.show()
--- test.py
+++ test.py
@@ -2,27 +2,28 @@ import time
 from matplotlib import pyplot as plt
 import tensorflow as tf
 
-def DeviceTest(device_name,size):
+def DeviceTest(device_name,size,sess):
     with tf.device(device_name):       
         M1=tf.random_normal([size,size],name='M1')
         M2=tf.random_normal([size,size],name='M2')
         mul=tf.matmul(M1,M2,name='mul')
         sum_result=tf.reduce_sum(mul,name='sum_result')
         start_time=time.time()
-        with tf.Session() as sess:
-            result=sess.run(sum_result)
+        result=sess.run(sum_result)
         take_time=time.time()-start_time       
-        print(device_name,"  size=",size, " Time:",take_time)
+        #print(device_name,"  size=",size, " Time:",take_time)
         return take_time  #這樣append到SET才有東西
 
-Gpu_Set=[];  Cpu_set=[];  Size_set=[]
-for i in range(0,10001,100):
-    TestGpu=DeviceTest("/gpu:0",i)
-    TestCpu=DeviceTest("/cpu:0",i)
-    print("-"*100)
-    Gpu_Set.append(TestGpu)
-    Cpu_set.append(TestCpu)
-    Size_set.append(i)
+with tf.Session() as sess:
+    Gpu_Set=[];  Cpu_set=[];  Size_set=[]
+    for i in range(0,2001,100):
+        TestGpu=DeviceTest("/gpu:0",i,sess)
+        TestCpu=DeviceTest("/cpu:0",i,sess)
+        #print("-"*100)
+        Gpu_Set.append(TestGpu)
+        Cpu_set.append(TestCpu)
+        Size_set.append(i)
+
 
 fig=plt.gcf()
 fig.set_size_inches(5,5)
